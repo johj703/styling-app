e4d4ca6c69954b5a04c85ebef5cec1478691c0ee
Jo <johj703@nate.com>
2024-08-16 21:41:54 +0900

    styled 컴포넌트에서 borderColor을 props로 return의 stBox 속성에 넣어주면 box의 border 색이 속성대로 변하는 것을 볼 수 있다.

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import styled from "styled-components";
 const StBox = styled.div`
   width: 100px;
   height: 100px;
-  border: 1px solid red;
+  border: 1px solid ${(props) => props.borderColor};
   margin: 20px;
 `;
 
 function App() {
   return (
     <>
-      <StBox>박스</StBox>
+      <StBox borderColor="red">빨간 박스</StBox>
+      <StBox borderColor="green">초록 박스</StBox>
+      <StBox borderColor="blue">파랑 박스</StBox>
     </>
   );
 }
