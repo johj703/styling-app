45bdf3594ade090964bea3b45eb1a679458e938a
Jo <johj703@nate.com>
2024-08-18 00:45:11 +0900

    count와 setCount를 props로 넘겨서 직접 button 태그에 onClick 함수를 직접 작성해서 구현할 수도 있다.

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,8 +1,12 @@
-const Child = () => {
+const Child = ({count, setCount}) => {
   return (
     <div>
       <h3>여기는 자식 컴포넌트 입니다.</h3>
-      <button>증가!</button>
+      <button
+        onClick={() => {
+            setCount(count + 1)
+        }}
+      >증가!</button>
     </div>
   )
 }
