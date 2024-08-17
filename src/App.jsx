bc541d1958fd53e6724c1917d4a354e3272a8b6b
Jo <johj703@nate.com>
2024-08-17 23:51:43 +0900

    button 태그 안에 직접 함수를 구현할 수도 있다.

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,14 @@ import { useState } from "react"
 
 const App = () => {
   const[count, setCount] = useState(0);
-  const handleCountPlus = () => {
-    setCount((현재값) => {
-      return 현재값 + 1;
-    });
-  }
 
   return (
     <div>
       {count}
       <br />
-      <button onClick={handleCountPlus}>증가</button>
+      <button onClick={() => {
+        setCount(count + 1);
+      }}>증가</button>
     </div>
   )
 }
