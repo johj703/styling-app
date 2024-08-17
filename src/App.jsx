d1ee4b8bd6c6f8ffa73b2dc5185a97ae6af11fa8
Jo <johj703@nate.com>
2024-08-18 00:34:46 +0900

    Child 컴포넌트 생성 후 app의 return에 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
+import Child from "./components/Child"
 
 const App = () => {
   const[count, setCount] = useState(0);
 
   return (
     <div>
-      
+      <h1>여기는 부모 컴포넌트 입니다.</h1>
+      <span>현재 카운트 : {count}</span>
+      <Child />
     </div>
   )
 }
---
+++ src/components/Child.jsx
@@ -0,0 +1,10 @@
+const Child = () => {
+  return (
+    <div>
+      <h3>여기는 자식 컴포넌트 입니다.</h3>
+      <button>중가!</button>
+    </div>
+  )
+}
+
+export default Child
