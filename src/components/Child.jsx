ed30bcaab5ffea434df09174a01fc59e5135b6d1
Jo <johj703@nate.com>
2024-08-18 23:56:46 +0900

    Child 컴포넌트 수정

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,14 +1,17 @@
-const Child = ({setCount}) => {
+/* eslint-disable react/prop-types */
+const Child = ({ setCount }) => {
   return (
     <div>
       <h3>여기는 자식 컴포넌트 입니다.</h3>
       <button
         onClick={() => {
-            setCount((prev) => prev + 1)
+          setCount((prev) => prev + 1);
         }}
-      >증가!</button>
+      >
+        증가!
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Child
+export default Child;
