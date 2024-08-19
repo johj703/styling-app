b83131db76bcd7ef3ddd60a364961aec16dec1d9
Jo <johj703@nate.com>
2024-08-19 21:54:37 +0900

    useRef로 선언한 값은 컴포넌트가 렌더링이 되어도 값을 유지한다!

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useRef } from "react";
+
 const App = () => {
+  // useRef로 선언한 값은 컴포넌트가 렌더링이 되어도 값을 유지한다!
+  const ref = useRef("초기 값");
+  console.log("ref", ref);
+
+  ref.current = "바꾼 값";
+  console.log("current => ", ref);
+
   return (
     <div>
       <h1>useRef</h1>
