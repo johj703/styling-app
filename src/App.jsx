6d5b15660be5f9ca2985a42c4256efe105e4d5b0
Jo <johj703@nate.com>
2024-08-19 21:47:52 +0900

    useRef는 rendering과는 상관이 없다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
-import { useEffect, useState } from "react";
-
 const App = () => {
-  const [value, setValue] = useState("");
-  const [count, setCount] = useState(0);
-
-  // 의존성 배열 : 이 배열에 값을 넣으면 그 값이 바뀔 때만 useEffect를 실행할께
-  useEffect(() => {
-    console.log("Hello useEffect!");
-  }, [count]);
   return (
     <div>
-      <h1>useEffect</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-      />
-      {count}
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        증가
-      </button>
+      <h1>useRef</h1>
     </div>
   );
 };
