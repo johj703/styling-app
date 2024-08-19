4621c6ef7acb8350e2ba89145e6301ff357d8bd9
Jo <johj703@nate.com>
2024-08-19 10:12:23 +0900

    useEffect를 위해 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from "react"
-import Child from "./components/Child"
-
 const App = () => {
-  const[count, setCount] = useState(0);
-
   return (
     <div>
-      <h1>여기는 부모 컴포넌트 입니다.</h1>
-      <span>현재 카운트 : {count}</span>
-      <Child setCount={setCount} />
+      <h1>useEffect</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
