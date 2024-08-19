2ca89d495f3b926ac857d758a1576989b36fba58
Jo <johj703@nate.com>
2024-08-19 20:10:17 +0900

    onChange에 setValue(e.target.value)를 걸어서 input에 글자를 입력할 때마다 Hello useEffect!가 호출되도록 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [value, setValue] = useState("");
+
   useEffect(() => {
     console.log("Hello useEffect!");
   });
   return (
     <div>
       <h1>useEffect</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
     </div>
   );
 };
