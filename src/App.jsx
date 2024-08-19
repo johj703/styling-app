dea8392add130c195973eee1d55f08c98ab8294e
Jo <johj703@nate.com>
2024-08-19 20:44:00 +0900

    useEffect에 의존성 배열을 빈 값을 입력하면 의존할 것이 없기 때문에 한번만 실행된다!

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { useEffect, useState } from "react";
 const App = () => {
   const [value, setValue] = useState("");
 
+  // 의존성 배열 : 이 배열에 값을 넣으면 그 값이 바뀔 때만 useEffect를 실행할께
   useEffect(() => {
     console.log("Hello useEffect!");
-  });
+  }, []);
   return (
     <div>
       <h1>useEffect</h1>
