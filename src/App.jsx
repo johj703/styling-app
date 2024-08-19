86085ae58ed3a88c024580828112ed38f1a7c92a
Jo <johj703@nate.com>
2024-08-19 22:21:44 +0900

    아이디, 비밀번호 입력하는 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import { useState, useRef } from "react";
-
 const App = () => {
-  const [count, setCount] = useState(0);
-  const countRef = useRef(0);
-
-  const plusStateCountButtonHandler = () => {
-    setCount(count + 1);
-  };
-  const plusRefCountButtonHandler = () => {
-    countRef.current++;
-  };
-
   return (
     <div>
-      <h1>useRef vs useState</h1>
       <div>
-        state영역 입니다. {count} <br />
-        <button onClick={plusStateCountButtonHandler}>state 증가</button>
+        아이디: <input type="text" />
       </div>
       <div>
-        ref영역 입니다. {countRef.current} <br />
-        <button onClick={plusRefCountButtonHandler}>ref 증가</button>
+        비밀번호: <input type="password" />
       </div>
     </div>
   );
