03e24ad965108ea188e82dcf76f643b553cde486
Jo <johj703@nate.com>
2024-08-19 20:47:16 +0900

    useEffect 두번 째 인자에 count 배열을 입력하게 되면 count의 값이 변경될 때마다 useEffect 안에 있는 로직이 실행된다.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 
 const App = () => {
   const [value, setValue] = useState("");
+  const [count, setCount] = useState(0);
 
   // 의존성 배열 : 이 배열에 값을 넣으면 그 값이 바뀔 때만 useEffect를 실행할께
   useEffect(() => {
     console.log("Hello useEffect!");
-  }, []);
+  }, [count]);
   return (
     <div>
       <h1>useEffect</h1>
@@ -17,6 +18,14 @@ const App = () => {
           setValue(e.target.value);
         }}
       />
+      {count}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        증가
+      </button>
     </div>
   );
 };
