d0a55129bc77d5305c53327014f514de593570e4
Jo <johj703@nate.com>
2024-08-19 22:25:46 +0900

    useEffect를 사용해서 컴포넌트가 처음 렌더링 될 때, 포커스가 아이디 input으로 가도록 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { useRef, useEffect } from "react";
+
 const App = () => {
+  const idRef = useRef("");
+
+  useEffect(() => {
+    idRef.current.focus();
+  });
+
   return (
     <div>
       <div>
-        아이디: <input type="text" />
+        아이디: <input type="text" ref={idRef} />
       </div>
       <div>
         비밀번호: <input type="password" />
