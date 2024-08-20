edb06a9800e52df17275633c016c8588c56a2d7d
Jo <johj703@nate.com>
2024-08-20 14:44:40 +0900

    text 수정

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -13,8 +13,8 @@ const Child = () => {
     <div>
       나는 이 집안의 막내에요.
       <br />
-      {/* 할아버지가 우리 집 이름은 <span style={stressWord}>{houseName}</span>라고 */}
-      하셨어요.
+      {/* 할아버지가 우리 집 이름은 <span style={stressWord}>{houseName}</span> */}
+      라고 하셨어요.
       <br />
       {/* 게다가 용돈도 <span style={stressWord}>{pocketMoney}</span>원 만큼이나 */}
       주셨답니다.
