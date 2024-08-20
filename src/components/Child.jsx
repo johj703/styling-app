00570b1b035ceb2cb532e0022e28e3c218179e62
Jo <johj703@nate.com>
2024-08-20 14:43:37 +0900

    주석 처리해서 화면에 text보이도록 작성

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -13,10 +13,10 @@ const Child = () => {
     <div>
       나는 이 집안의 막내에요.
       <br />
-      할아버지가 우리 집 이름은 <span style={stressWord}>{houseName}</span>라고
+      {/* 할아버지가 우리 집 이름은 <span style={stressWord}>{houseName}</span>라고 */}
       하셨어요.
       <br />
-      게다가 용돈도 <span style={stressWord}>{pocketMoney}</span>원 만큼이나
+      {/* 게다가 용돈도 <span style={stressWord}>{pocketMoney}</span>원 만큼이나 */}
       주셨답니다.
     </div>
   );
