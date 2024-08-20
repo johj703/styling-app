f756fca166d20de98664f823da45cb5ee2e36f9a
Jo <johj703@nate.com>
2024-08-20 14:42:36 +0900

    FamilyContext뒤에 .Provider를 추가해야 한다

--- src/components/GrandFather.jsx
+++ src/components/GrandFather.jsx
@@ -6,14 +6,14 @@ const GrandFather = () => {
   const pocketMoney = 10000;
 
   return (
-    <FamilyContext
+    <FamilyContext.Provider
       value={{
         houseName,
         pocketMoney,
       }}
     >
       <Father />
-    </FamilyContext>
+    </FamilyContext.Provider>
   );
 };
 
