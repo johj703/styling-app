438bfb6c792f9f52994e505d003245241140a67c
Jo <johj703@nate.com>
2024-08-20 14:41:01 +0900

    GrandFather와 Child 사이에서 데이터 전달만 했던 Father 컴포넌트의 props는 삭제

--- src/components/Father.jsx
+++ src/components/Father.jsx
@@ -1,7 +1,7 @@
 import Child from "./Child";
 
-const Father = ({ houseName, pocketMoney }) => {
-  return <Child houseName={houseName} pocketMoney={pocketMoney} />;
+const Father = () => {
+  return <Child />;
 };
 
 export default Father;
